6511544806c6c9ffc1164405dc8040bd5c758861
BRPersonal <balajirengan@gmail.com>
2025-05-17 10:32:38 +0530

    Just added a comment

--- src/tasks/dto/update-task-status.dto.ts
+++ src/tasks/dto/update-task-status.dto.ts
@@ -1,6 +1,8 @@
 import { IsEnum } from 'class-validator';
 import { TaskStatus } from '../task.model';
 
+//The only reason we have a dto for single field
+//is that isEnum decorator can only be applied on a dto
 export class UpdateTaskStatusDto {
   @IsEnum(TaskStatus)
   status: TaskStatus;
