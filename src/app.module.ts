10dd3b0fff430f8b01b9e2bc0f3669116b5d82e3
BRPersonal <balajirengan@gmail.com>
2025-05-19 23:14:40 +0530

    auth in progress

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TasksModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
@@ -14,7 +17,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       database: 'task-management',
       autoLoadEntities: true,
       synchronize: true
-    })    
+    }),
+    AuthModule    
   ]
 
 })
