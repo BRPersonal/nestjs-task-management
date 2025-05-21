ce42ac1decdd29b8ab095a3217864058b9984541
BRPersonal <balajirengan@gmail.com>
2025-05-21 14:18:56 +0530

    authorization in progress

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,12 +1,12 @@
 import { Controller,Get,Post,Delete,Patch,Body,Param,Query,UseGuards } from '@nestjs/common';
-import { IsEnum } from 'class-validator';
 import { TasksService} from './tasks.service';
 import {Task} from './task.entity';
-import {TaskStatus} from './task-status.enum';
+import { User } from 'src/auth/user.entity';
 import {CreateTaskDto} from './dto/create-task.dto';
 import {GetTasksFilterDto} from './dto/get-tasks-filter.dto';
 import {UpdateTaskStatusDto} from './dto/update-task-status.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from 'src/auth/get-user.decorator';
 
 @Controller('/tasks')
 @UseGuards(AuthGuard())
@@ -16,35 +16,37 @@ export class TasksController {
     }
 
     @Get('/:id')
-    public async getTaskById(@Param('id') id: string): Promise<Task> {
-      return await this.service.getTaskById(id);
+    public async getTaskById(@Param('id') id: string,@GetUser() user: User): Promise<Task> {
+      return await this.service.getTaskById(id,user);
     }
 
     @Get()
-    public async getTasks(@Query() filterDto : GetTasksFilterDto) : Promise<Task[]> {
+    public async getTasks(@Query() filterDto : GetTasksFilterDto,
+                        @GetUser() user: User ) : Promise<Task[]> {
 
-        return await this.service.getTasks(filterDto);
+        return await this.service.getTasks(filterDto,user);
     }
 
     @Post()
     public async createTask(
-        @Body() createTaskDto : CreateTaskDto) : Promise<Task>
+        @Body() createTaskDto : CreateTaskDto,@GetUser() user: User) : Promise<Task>
     {
-        return await this.service.createTask(createTaskDto);
+        return await this.service.createTask(createTaskDto,user);
     }
 
     @Delete('/:id')
-    public async deleteTask(@Param('id') id:string):Promise<void> {
-        await this.service.deleteTask(id);
+    public async deleteTask(@Param('id') id:string,@GetUser() user: User):Promise<void> {
+        await this.service.deleteTask(id,user);
 
     }
 
     @Patch('/:id/status')
     public async updateTaskStatus(
         @Param('id') id:string,
-        @Body() statusDto:UpdateTaskStatusDto) : Promise<Task> {
+        @Body() statusDto:UpdateTaskStatusDto,
+        @GetUser() user: User) : Promise<Task> {
             const {status} = statusDto;
-            return await this.service.updateTaskStatus(id,status);
+            return await this.service.updateTaskStatus(id,status,user);
 
     }
 }
